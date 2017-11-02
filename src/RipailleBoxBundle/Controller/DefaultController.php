48a3fbad45b5ef05eb9e8d6c2b11d482d26dc9c7
Guillaume Orain <louveciennes.gui_appa@yahoo.fr>
2017-11-02 10:11:19 +0100

    création du bundle RipailleBox

--- src/RipailleBoxBundle/Controller/DefaultController.php
+++ src/RipailleBoxBundle/Controller/DefaultController.php
@@ -1,21 +1,22 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace RipailleBoxBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController
+ * @package RipailleBoxBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('RipailleBoxBundle::index.html.twig');
     }
 }
---
+++ src/RipailleBoxBundle/RipailleBoxBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RipailleBoxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RipailleBoxBundle
+ * @package RipailleBoxBundle
+ */
+class RipailleBoxBundle extends Bundle
+{
+}
